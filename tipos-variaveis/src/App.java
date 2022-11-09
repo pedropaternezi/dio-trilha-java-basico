f27e9042ec45d4c41dd4bc7a752fefcc6282da72
pedropaternezi <pedro.lspaternezi@gmail.com>
2022-11-09 17:19:03 -0300

    Exercicio SmartTV

---
+++ tipos-variaveis/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) {
+        int a, b;
+        a = 40;
+        b = 60;
+        c = 100;
+        
+
+        String resultado = a+b ? "igual" : "diferente";
+
+        System.out.println(resultado);
+    }
+}
